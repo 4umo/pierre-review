32c357c520ccdae8da57fd5db069363dc96ad584
Austin Montgomery <austin.m.montgomery@gmail.com>
2023-08-26 15:58:03 -0400

    print env

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
+# from github import Github, Auth
 
 def main():
     # read env
+    print(os.environ)
+
+    # setup client
+    # auth = Auth.Token("access_token")
+    # GH = Github(auth=auth)
 
     # fetch the diff
 
     # send to langchain
 
     # write a comment/description
-    print('Hello world!')
+    print('Hello World!')
     return 0
 
 if __name__ == "__main__":
