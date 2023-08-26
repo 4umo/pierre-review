5af524d3deae48d1b8a402d0dec8c947c08ba56e
Austin Montgomery <austin.m.montgomery@gmail.com>
2023-08-26 16:17:21 -0400

    env parsing to main

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import os
-# from github import Github, Auth
+from github import Github, Auth
 
 def main():
     # read env
     print(os.environ)
+    
+    GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN', None)
+    LANGCHAIN_LLM_API_TOKEN = os.environ.get('LANGCHAIN_LLM_API_TOKEN', None)
+    LANGCHAIN_LLM_KEY = os.environ.get('LANGCHAIN_LLM_KEY', None)
+    
+    if not GITHUB_TOKEN or not LANGCHAIN_LLM_API_TOKEN or not LANGCHAIN_LLM_KEY:
+        print(f"""Missing environment variables:
+        {'GITHUB_TOKEN' if not GITHUB_TOKEN else ''}
+        {'LANGCHAIN_LLM_API_TOKEN' if not LANGCHAIN_LLM_API_TOKEN else ''}
+        {'LANGCHAIN_LLM_KEY' if not LANGCHAIN_LLM_KEY else ''}
+        """)
+        return 1
 
     # setup client
-    # auth = Auth.Token("access_token")
-    # GH = Github(auth=auth)
+    G = Github(auth=Auth.Token(GITHUB_TOKEN))
 
     # fetch the diff
 
