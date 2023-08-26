c577371c2da2b8163cf7dc286d51429294568c7d
Kevin Pan <kevinrpan@gmail.com>
2023-08-26 15:06:51 -0400

    minimal print main + update reqs

--- main.py
+++ main.py
@@ -1,47 +1,23 @@
 import os
 import json
 import requests
-from prompt import generate_prompt, generate_comment
+from prompt import generate_comment
 
 ## FILLER FROM AUTOCOMPLETE
 
-def get_pull_request_info():
-    # Fetch the pull request info from GitHub API
-    # (Replace with actual API calls)
-    return {
-        'id': 1,
-        'diff_url': 'https://api.github.com/repos/owner/repo/pulls/1.diff'
-    }
-
-def post_comment_to_pr(pr_id, comment):
-    # Post a comment to the pull request using GitHub API
-    # (Replace with actual API calls)
-    api_url = f"https://api.github.com/repos/owner/repo/pulls/{pr_id}/comments"
-    headers = {
-        'Authorization': f"token {os.getenv('GITHUB_TOKEN')}",
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    payload = {
-        'body': comment
-    }
-    response = requests.post(api_url, headers=headers, data=json.dumps(payload))
-    if response.status_code == 201:
-        print(f"Successfully posted comment to PR {pr_id}")
-
 def main():
-    # Fetch pull request info
-    pr_info = get_pull_request_info()
-    pr_id = pr_info['id']
-    diff_url = pr_info['diff_url']
-
-    # Get code difference
-    prompt = generate_prompt(diff_url)
-
-    # Generate comments based on code difference
-    comment = generate_comment(prompt)
+    print("Hello World")
+    # comment = generate_comment(prompt)
 
     # Post each comment to the pull request
-    post_comment_to_pr(pr_id, comment)
+    # post_comment_to_pr(pr_id, comment)
 
 if __name__ == "__main__":
     main()
+    # GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")  # Replace with your GitHub token
+    # REPO_NAME = "owner/repo"  # Replace with your repository name "owner/repo"
+    # PR_NUMBER = 1  # Replace with your pull request number
+    
+    # diff_content = get_pr_diff(REPO_NAME, PR_NUMBER, GITHUB_TOKEN)
+    # print("Diff Content:")
+    # print(diff_content)
