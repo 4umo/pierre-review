2681528fdb48df01304ae674fbabf5fb5fd5b622
Austin Montgomery <austin.m.montgomery@gmail.com>
2023-08-26 15:46:25 -0400

    clean main

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import os
-import json
-import requests
-from prompt import generate_comment
-
-## FILLER FROM AUTOCOMPLETE
 
 def main():
-    print("Hello World")
-    # comment = generate_comment(prompt)
+    # read env
 
-    # Post each comment to the pull request
-    # post_comment_to_pr(pr_id, comment)
+    # fetch the diff
+
+    # send to langchain
+
+    # write a comment/description
+    return 0
 
 if __name__ == "__main__":
     main()
-    # GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")  # Replace with your GitHub token
-    # REPO_NAME = "owner/repo"  # Replace with your repository name "owner/repo"
-    # PR_NUMBER = 1  # Replace with your pull request number
-    
-    # diff_content = get_pr_diff(REPO_NAME, PR_NUMBER, GITHUB_TOKEN)
-    # print("Diff Content:")
-    # print(diff_content)
